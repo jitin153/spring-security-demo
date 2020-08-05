9f92f6894a80992a3c9ed7f11e4108d0a47bf076
Jitin Kumar <jitingangwar@gmail.com>
2020-08-05 15:10:27 +0530

    Corrected the mistake.

--- src/test/java/com/demospringsecurity/ControllerTest.java
+++ src/test/java/com/demospringsecurity/ControllerTest.java
@@ -1,35 +1,38 @@
 package com.demospringsecurity;
 
-import com.demospringsecurity.controller.MyTestController;
-import com.demospringsecurity.service.TestService;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.stubbing.OngoingStubbing;
-
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.demospringsecurity.controller.MyTestController;
+import com.demospringsecurity.controller.MyTestControllerImpl;
+import com.demospringsecurity.service.TestService;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
 public class ControllerTest {
-	private MyTestController myTestController;
+	//@Autowired
+	//private MyTestController myTestController;
+	private MyTestController myTestController = new MyTestControllerImpl();
 	private TestService testService = mock(TestService.class);
 
 	@Before
 	public void setup() {
-
+		ReflectionTestUtils.setField(myTestController, "testService", testService);
 	}
 
 	@Test
 	public void testGetNotification() {
 		List<String> nots=Arrays.asList("N1","N2","N3");
 		when(testService.getNotification()).thenReturn(nots);
-		List<String> outFromService = testService.getNotification();
+		List<String> outFromService = myTestController.getNotification();
 		assertNotNull(outFromService);
 		assertEquals(3,outFromService.size());
 		assertEquals("N2",outFromService.get(1));
